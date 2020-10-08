328ce65e5dd970fdc6a80c3050973d8b2688e7c0
John S Watson <JohnSWatson@users.noreply.github.com>
2020-10-08 14:40:51 +1100

    Add project files.

---
+++ Viewer.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace AforgeCameraSettings
+{
+    partial class Viewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Viewer));
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox1CameraList = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripMenuItemCameraSettings = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripContainer1
+            // 
+            this.toolStripContainer1.BottomToolStripPanelVisible = false;
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.pictureBox1);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(800, 418);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.LeftToolStripPanelVisible = false;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.RightToolStripPanelVisible = false;
+            this.toolStripContainer1.Size = new System.Drawing.Size(800, 450);
+            this.toolStripContainer1.TabIndex = 0;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.menuStrip1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 418);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toolStripComboBox1CameraList,
+            this.toolStripMenuItemCameraSettings,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 32);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 28);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // toolStripComboBox1CameraList
+            // 
+            this.toolStripComboBox1CameraList.Name = "toolStripComboBox1CameraList";
+            this.toolStripComboBox1CameraList.Size = new System.Drawing.Size(200, 28);
+            this.toolStripComboBox1CameraList.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox1CameraList_SelectedIndexChanged);
+            // 
+            // toolStripMenuItemCameraSettings
+            // 
+            this.toolStripMenuItemCameraSettings.Name = "toolStripMenuItemCameraSettings";
+            this.toolStripMenuItemCameraSettings.Size = new System.Drawing.Size(131, 28);
+            this.toolStripMenuItemCameraSettings.Text = "Camera Settings";
+            this.toolStripMenuItemCameraSettings.Click += new System.EventHandler(this.toolStripMenuItemCameraSettings_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(64, 28);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Viewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStripContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Viewer";
+            this.Text = "Aforge Camera Settings";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Viewer_FormClosing);
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1CameraList;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemCameraSettings;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ Viewer.cs
@@ -0,0 +1,208 @@
+﻿namespace AforgeCameraSettings
+{
+    using AForge.Video;
+    using AForge.Video.DirectShow;
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Defines the <see cref="Viewer" />.
+    /// </summary>
+    public partial class Viewer : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Viewer"/> class.
+        /// </summary>
+        public Viewer()
+        {
+            InitializeComponent();
+
+            LoadCameraList();
+        }
+
+        // List of filterInfo for each attached camera
+        // No good pluging a camera in after the app has started
+        /// <summary>
+        /// Defines the filterInfo.
+        /// </summary>
+        List<FilterInfo> filterInfo = new List<FilterInfo>();
+
+        // The indiex in above list for he selected camera
+        /// <summary>
+        /// Defines the cameraIndex.
+        /// </summary>
+        int cameraIndex = -1;// Not set
+
+        /// <summary>
+        /// Defines the videoSource.
+        /// </summary>
+        VideoCaptureDevice videoSource;
+
+        /// <summary>
+        /// The toolStripComboBoxCameraSelection_TextChanged.
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/>.</param>
+        /// <param name="e">The e<see cref="EventArgs"/>.</param>
+        private async void toolStripComboBoxCameraSelection_TextChanged(object sender, EventArgs e)
+        {
+            cameraIndex = toolStripComboBox1CameraList.SelectedIndex;
+            await StartCamera(cameraIndex);
+        }
+
+        /// <summary>
+        /// The LoadCameraList.
+        /// </summary>
+        private void LoadCameraList()
+        {
+            FilterInfoCollection videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            for (int i = 0; i < videoDevices.Count; i++)
+            {
+                if (!(videoDevices[i].Name == "Logi Capture"))
+                {
+                    filterInfo.Add(videoDevices[i]);
+                    toolStripComboBox1CameraList.Items.Add(videoDevices[i].Name);
+                }
+            }
+        }
+
+        /// <summary>
+        /// The StartCamera.
+        /// </summary>
+        /// <param name="index">The index<see cref="int"/>.</param>
+        /// <returns>The <see cref="Task"/>.</returns>
+        private async Task StartCamera(int index)
+        {
+            StopCamera();  // Try it  to be sure to be sure
+
+            // create video source
+            videoSource = new VideoCaptureDevice(filterInfo[cameraIndex].MonikerString);
+
+            // set NewFrame event handler
+            videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+            // start the video source
+            videoSource.Start();
+            Task<int> i = waitabit();
+        }
+
+        /// <summary>
+        /// The waitabit.
+        /// </summary>
+        /// <returns>The <see cref="Task{int}"/>.</returns>
+        private async Task<int> waitabit()
+        {
+            await Task.Delay(2000);
+            return 1;
+        }
+
+        /// <summary>
+        /// The StopCamera.
+        /// </summary>
+        private void StopCamera()
+        {
+            if (!(videoSource == null))
+            {
+                if (videoSource.IsRunning)
+                {
+                    videoSource.SignalToStop();
+                    videoSource.WaitForStop();
+                }
+            }
+        }
+
+        /// <summary>
+        /// The Cameraparameters.
+        /// </summary>
+        private void Cameraparameters()
+        {
+            if (!(videoSource == null))
+            {
+                if (videoSource.IsRunning)
+                {
+                    videoSource.DisplayPropertyPage(IntPtr.Zero);
+                    var x = videoSource.VideoCapabilities;
+                    var y = videoSource.VideoResolution;
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// The video_NewFrame.
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/>.</param>
+        /// <param name="eventArgs">The eventArgs<see cref="NewFrameEventArgs"/>.</param>
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bm = AForge.Imaging.Image.Clone(eventArgs.Frame);
+            pictureBox1.Image = bm;
+        }
+
+        /// <summary>
+        /// The toolStripMenuItem1Camerasettings_Click.
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/>.</param>
+        /// <param name="e">The e<see cref="EventArgs"/>.</param>
+        private void toolStripMenuItem1Camerasettings_Click(object sender, EventArgs e)
+        {
+            Cameraparameters();
+        }
+
+        // End Camera stuff
+        /// <summary>
+        /// The exitToolStripMenuItem_Click.
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/>.</param>
+        /// <param name="e">The e<see cref="EventArgs"/>.</param>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+            StopCamera();
+        }
+
+        /// <summary>
+        /// The toolStripMenuItemCameraSettings_Click.
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/>.</param>
+        /// <param name="e">The e<see cref="EventArgs"/>.</param>
+        private void toolStripMenuItemCameraSettings_Click(object sender, EventArgs e)
+        {
+            Cameraparameters();
+        }
+
+        /// <summary>
+        /// The toolStripComboBox1CameraList_SelectedIndexChanged.
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/>.</param>
+        /// <param name="e">The e<see cref="EventArgs"/>.</param>
+        private async void toolStripComboBox1CameraList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cameraIndex = toolStripComboBox1CameraList.SelectedIndex;
+            await StartCamera(cameraIndex);
+        }
+
+        /// <summary>
+        /// The Viewer_FormClosing.
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/>.</param>
+        /// <param name="e">The e<see cref="FormClosingEventArgs"/>.</param>
+        private void Viewer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StopCamera();
+        }
+
+        /// <summary>
+        /// The aboutToolStripMenuItem_Click.
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/>.</param>
+        /// <param name="e">The e<see cref="EventArgs"/>.</param>
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form about = new About();
+
+            about.ShowDialog();
+        }
+    }
+}
